e273050f6694a3f7991e938ebb179198b6411423
Karangwa <gahima.ronald@yahoo.com>
2023-01-26 08:46:40 +0000

    Return a Promise using this prototype function getResponseFromAPI()

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,19 @@
+export default function getResponseFromAPI() {
+
+  const success = true;
+
+  return new Promise((resolve, reject) => {
+
+    if (success) {
+
+      resolve();
+
+    } else {
+
+      reject();
+
+    }
+
+  });
+
+}
